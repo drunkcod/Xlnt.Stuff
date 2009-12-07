bc30d14aa858718a8abb8d11fe18a9bebab1d027
Torbjorn Gyllebring <i.am@drunkencoder.com>
2009-12-07 16:08:07 +0100

    FNUnit moved to Stuff.

---
+++ Source/FNUnit.fs
@@ -0,0 +1,11 @@
+﻿namespace FNUnit
+open NUnit.Framework
+
+type FactAttribute = NUnit.Framework.TestAttribute
+type SetupAttribute = NUnit.Framework.SetUpAttribute
+
+[<AutoOpen>]
+module FNunitExtensions =
+    let should what actual = what actual
+    let be (expected:obj) actual = Assert.AreEqual(expected, actual)
+    let contain wanted collection = Assert.Contains(wanted, collection)
