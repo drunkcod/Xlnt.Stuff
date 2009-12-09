d3465d4502e926057c2b0ac25a82000c13ebf379
unknown <drunkcod@.(none)>
2009-12-09 21:06:33 +0100

    TypeExtensions.ConstructAs added.

--- Source/ObjectExtensions.cs
+++ Source/ObjectExtensions.cs
@@ -5,14 +5,12 @@ namespace Xlnt.Stuff
 {
     public static class ObjectExtensions
     {
-        public static IEnumerable<object> Select<T>(this T obj, IEnumerable<Func<T, object>> funcs)
-        {
+        public static IEnumerable<object> Select<T>(this T obj, IEnumerable<Func<T, object>> funcs){
             foreach(var fun in funcs)
                 yield return fun(obj);
         }
 
-        public static IEnumerable<object> Select<T>(this T obj, params Func<T, object>[] funcs)
-        {
+        public static IEnumerable<object> Select<T>(this T obj, params Func<T, object>[] funcs){
             return Select<T>(obj, (IEnumerable<Func<T, object>>)funcs);
         }
     }
---
+++ Tests/TypeExtensionsTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Xlnt.Stuff
+{
+    public class TypeExtensionsTests
+    {
+        class MyClass
+        {
+            public const int DefaultValue = 42;
+
+            readonly int value;
+
+            public MyClass(): this(MyClass.DefaultValue){}
+            public MyClass(int value) { this.value = value; }
+
+            public int Value { get { return value; } }
+        }
+
+        [Test]
+        public void ConstructAs_should_use_default_ctor_when_no_parameters_given() {
+            var obj = typeof(MyClass).ConstructAs<MyClass>();
+            Assert.That(obj.Value, Is.EqualTo(MyClass.DefaultValue));
+        }
+        [Test]
+        public void ConstructAs_should_find_parameterized_ctor() {
+            var value = MyClass.DefaultValue + 1;
+            var obj = typeof(MyClass).ConstructAs<MyClass>(value);
+            Assert.That(obj.Value, Is.EqualTo(value));
+        
+        }
+
+    }
+}
