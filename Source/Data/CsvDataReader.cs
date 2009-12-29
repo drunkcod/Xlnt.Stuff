85d9332a3d32921e69392b19b158f12cae4271f2
Torbjorn Gyllebring <i.am@drunkencoder.com>
2009-12-29 10:29:51 +0100

    Split to file per class.

---
+++ Source/Data/CsvDataReader.cs
@@ -0,0 +1,194 @@
+using System;
+using System.Data;
+using System.IO;
+
+namespace Xlnt.Tests.Data
+{
+    class CsvDataReader : IDataReader
+    {
+        readonly TextReader reader;
+        string[] values;
+        
+        public CsvDataReader(TextReader reader){
+            this.reader = reader;    
+        }
+
+        #region IDataReader Members
+
+        public void Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Depth
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public DataTable GetSchemaTable()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsClosed
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool NextResult()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Read()
+        {
+            var line = reader.ReadLine();
+            if(string.IsNullOrEmpty(line))
+                return false;
+            values = line.Split(',');
+            return true;
+        }
+
+        public int RecordsAffected
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        #endregion
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region IDataRecord Members
+
+        public int FieldCount { get; set; }
+
+        public bool GetBoolean(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte GetByte(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long GetBytes(int i, long fieldOffset, byte[] buffer, int bufferoffset, int length)
+        {
+            throw new NotImplementedException();
+        }
+
+        public char GetChar(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long GetChars(int i, long fieldoffset, char[] buffer, int bufferoffset, int length)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataReader GetData(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetDataTypeName(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime GetDateTime(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetDecimal(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetDouble(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Type GetFieldType(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public float GetFloat(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Guid GetGuid(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public short GetInt16(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetInt32(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long GetInt64(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetName(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetOrdinal(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetString(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetValue(int i)
+        {
+            return values[i];
+        }
+
+        public int GetValues(object[] values)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsDBNull(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object this[string name]
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public object this[int i]
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        #endregion
+    }
+}
---
+++ Tests/Data/CsvDataReaderTests.cs
@@ -0,0 +1,22 @@
+using System.IO;
+using NUnit.Framework;
+
+namespace Xlnt.Tests.Data
+{
+    public class CsvDataReaderTests
+    {
+        [Test]
+        public void cant_Read_from_empty_stream(){
+            var csv = new CsvDataReader(new StringReader(string.Empty));
+            Assert.That(csv.Read(), Is.False);
+        }
+
+        [Test]
+        public void GetValue_returns_fields_in_order(){
+            var csv = new CsvDataReader(new StringReader("1,2,3"));
+            csv.Read();
+
+            Assert.That(new[]{ csv.GetValue(0), csv.GetValue(1), csv.GetValue(2)}, Is.EqualTo(new[]{"1", "2", "3"}));            
+        }
+    }
+}
