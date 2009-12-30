e3b9f6bdcd3a1ab8731e2133b5a674d2a7b1a7dc
Torbjorn Gyllebring <i.am@drunkencoder.com>
2009-12-30 20:35:04 +0100

    LineReader moved to own file.

--- Source/Data/CsvDataReader.cs
+++ Source/Data/CsvDataReader.cs
@@ -8,16 +8,7 @@ namespace Xlnt.Data
 {
     public class CsvDataReader : IDataReader
     {
-        class LineReader : ILineReader
-        {
-            readonly TextReader reader;
-            public LineReader(TextReader reader){
-                this.reader =reader;
-            }
 
-            public string ReadLine() { return reader.ReadLine(); }
-            public void Dispose(){ reader.Dispose();}
-        }
 
         static readonly char[] DefaultSeparator = new[]{','};
 
---
+++ Source/IO/LineReader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace Xlnt.IO
+{
+    class LineReader : ILineReader
+    {
+        readonly TextReader reader;
+        public LineReader(TextReader reader) {
+            this.reader = reader;
+        }
+
+        public string ReadLine() { return reader.ReadLine(); }
+        public void Dispose() { reader.Dispose(); }
+    }
+}
