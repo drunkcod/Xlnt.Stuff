375f1d36255d3e883f762a21a924b77689c9f453
Torbjorn Gyllebring <i.am@drunkencoder.com>
2010-01-04 17:30:40 +0100

    ?

--- Tests/Data/CsvReportTests.cs
+++ Tests/Data/CsvReportTests.cs
@@ -12,46 +12,50 @@ namespace Xlnt.Tests.Data
             public string Value;
         }
 
+        private TextWriter target;
+        private CsvReport<ReportLine> report;
+
+        [SetUp]
+        public void Setup(){
+            target = new StringWriter();
+            report = new CsvReport<ReportLine>(target);
+        }
+
         [Test]
         public void Map_columns_for_via_Expressions(){
-            var target = new StringWriter();
-            var csv = new CsvReport<ReportLine>(target)
-                .Map(x => x.Id)
+            report.Map(x => x.Id)
                 .Map(x => x.Value);
-            csv.WriteAll(new[] {new ReportLine {Id = 42, Value = "The Answer"}});
+
+            report.WriteAll(new[] {new ReportLine {Id = 42, Value = "The Answer"}});
 
             Assert.That(target.ToString(), Is.EqualTo("42;The Answer"));
         }
         [Test]
         public void Map_columns_via_names_and_lambdas(){
-            var target = new StringWriter();
-            var csv = new CsvReport<ReportLine>(target)
-                .Map("Foo", x => x.Id)
+            report.Map("Foo", x => x.Id)
                 .Map("Bar", x => x.Value);
-            csv.WriteHeader = true;
-            csv.WriteAll(new[] { new ReportLine { Id = 42, Value = "The Answer" } });
 
-            Assert.That(target.ToString(), Is.EqualTo("Foo;Bar\r\n42;The Answer"));
-            
+            report.WriteHeader = true;
+            report.WriteAll(new[] { new ReportLine { Id = 42, Value = "The Answer" } });
+
+            Assert.That(target.ToString(), Is.EqualTo("Foo;Bar\r\n42;The Answer"));            
         }
         [Test]
         public void Expression_columns_generate_header_named_after_their_fields_or_properties(){
-            var target = new StringWriter();
-            var csv = new CsvReport<ReportLine>(target)
-                .Map(x => x.Id)
+            report.Map(x => x.Id)
                 .Map(x => x.Value);
-            csv.WriteHeader = true;
-            csv.WriteAll(new[] { new ReportLine { Id = 42, Value = "The Answer" } });
+
+            report.WriteHeader = true;
+            report.WriteAll(new[] { new ReportLine { Id = 42, Value = "The Answer" } });
 
             Assert.That(target.ToString(), Is.StringStarting("Id;Value"));            
         }
         [Test]
         public void should_write_one_record_per_line(){
-            var target = new StringWriter();
-            var csv = new CsvReport<ReportLine>(target)
-                .Map(x => x.Id)
+            report.Map(x => x.Id)
                 .Map(x => x.Value);
-            csv.WriteAll(new[]{
+
+            report.WriteAll(new[]{
                 new ReportLine { Id = 1, Value = "First" },
                 new ReportLine { Id = 2, Value = "Second"}});
 
