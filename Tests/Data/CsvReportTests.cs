b6f21ae8d304e666e8e38f92195364b7c403973c
Torbjorn Gyllebring <i.am@drunkencoder.com>
2010-01-04 12:34:42 +0100

    Write column headers.

--- Tests/Data/CsvReportTests.cs
+++ Tests/Data/CsvReportTests.cs
@@ -8,30 +8,52 @@ namespace Xlnt.Tests.Data
 {
     class CsvReport<T>
     {
+        struct CsvReportColumn
+        {
+            public string Name;
+            public Func<T, string> GetValue;
+        }
         readonly TextWriter target;
-        readonly List<Func<T,string>> columns= new List<Func<T, string>>();
+        readonly List<CsvReportColumn> columns = new List<CsvReportColumn>();
 
         public CsvReport(TextWriter target){
             this.target = target;
         }
 
+        public bool WriteHeader { get; set; }
+
         public CsvReport<T> Map<TAny>(Expression<Func<T,TAny>> column){
             var get = column.Compile();
-            columns.Add(x => get(x).ToString());
+            
+            columns.Add(new CsvReportColumn{
+                Name = GetName(column),
+                GetValue = x => get(x).ToString()
+            });
             return this;
         }
 
-        public void WriteAll(IEnumerable<T> collection)
-        {
+        public void WriteAll(IEnumerable<T> collection){
+            WriteHeaders();
             foreach (var item in collection)
                 WriteRecord(item);
         }
 
-        void WriteRecord(T item)
-        {
+        void WriteHeaders(){
+            if(!WriteHeader)
+                return;
+            var delimiter = "";
+            for (var i = 0; i != columns.Count; ++i, delimiter = ";")
+                target.Write("{0}{1}", delimiter, columns[i].Name);
+        }
+
+        void WriteRecord(T item){
             var delimiter = "";
             for(var i = 0; i != columns.Count; ++i, delimiter = ";")
-                target.Write("{0}{1}", delimiter, columns[i](item));
+                target.Write("{0}{1}", delimiter, columns[i].GetValue(item));
+        }
+
+        static string GetName<TAny>(Expression<Func<T,TAny>> column){
+            return ((MemberExpression)column.Body).Member.Name;
         }
     }
 
@@ -52,6 +74,17 @@ namespace Xlnt.Tests.Data
             csv.WriteAll(new[] {new ReportLine {Id = 42, Value = "The Answer"}});
 
             Assert.That(target.ToString(), Is.EqualTo("42;The Answer"));
+        }
+        [Test]
+        public void Expression_columns_generate_header_named_after_their_fields_or_properties(){
+            var target = new StringWriter();
+            var csv = new CsvReport<ReportLine>(target)
+                .Map(x => x.Id)
+                .Map(x => x.Value);
+            csv.WriteHeader = true;
+            csv.WriteAll(new[] { new ReportLine { Id = 42, Value = "The Answer" } });
+
+            Assert.That(target.ToString(), Is.StringStarting("Id;Value"));            
         }
     }
 }
