2ed5b3088fab72bebf8f06ac3e59c15d1b563cd1
Torbjorn Gyllebring <i.am@drunkencoder.com>
2010-01-04 12:49:39 +0100

    Line per record.

--- Tests/Data/CsvReportTests.cs
+++ Tests/Data/CsvReportTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq.Expressions;
 using NUnit.Framework;
+using Xlnt.Stuff;
 
 namespace Xlnt.Tests.Data
 {
@@ -13,8 +14,13 @@ namespace Xlnt.Tests.Data
             public string Name;
             public Func<T, string> GetValue;
         }
-        readonly TextWriter target;
+
+        const string FieldDelimiter = ";";
+        static string RecordDelimiter { get { return Environment.NewLine; } }
+
+            readonly TextWriter target;
         readonly List<CsvReportColumn> columns = new List<CsvReportColumn>();
+        private string delimiter;
 
         public CsvReport(TextWriter target){
             this.target = target;
@@ -34,23 +40,26 @@ namespace Xlnt.Tests.Data
 
         public void WriteAll(IEnumerable<T> collection){
             WriteHeaders();
-            foreach (var item in collection)
-                WriteRecord(item);
+            collection.Each(WriteRecord);
         }
 
         void WriteHeaders(){
             if(!WriteHeader)
                 return;
-            var delimiter = "";
-            for (var i = 0; i != columns.Count; ++i, delimiter = ";")
-                target.Write("{0}{1}", delimiter, columns[i].Name);
+            WriteColumns(x => x.Name);
         }
 
         void WriteRecord(T item){
-            var delimiter = "";
-            for(var i = 0; i != columns.Count; ++i, delimiter = ";")
-                target.Write("{0}{1}", delimiter, columns[i].GetValue(item));
+            WriteColumns(x => x.GetValue(item));
         }
+
+        void WriteColumns(Func<CsvReportColumn,string> getValue){
+            for (var i = 0; i != columns.Count; ++i, delimiter = FieldDelimiter)
+                target.Write("{0}{1}", delimiter, getValue(columns[i]));
+            NextRecord();            
+        }
+
+        void NextRecord() { delimiter = RecordDelimiter; }
 
         static string GetName<TAny>(Expression<Func<T,TAny>> column){
             return ((MemberExpression)column.Body).Member.Name;
@@ -86,5 +95,18 @@ namespace Xlnt.Tests.Data
 
             Assert.That(target.ToString(), Is.StringStarting("Id;Value"));            
         }
+        [Test]
+        public void should_write_one_record_per_line(){
+            var target = new StringWriter();
+            var csv = new CsvReport<ReportLine>(target)
+                .Map(x => x.Id)
+                .Map(x => x.Value);
+            csv.WriteAll(new[]{
+                new ReportLine { Id = 1, Value = "First" },
+                new ReportLine { Id = 2, Value = "Second"}});
+
+            Assert.That(target.ToString(), Is.EqualTo("1;First\r\n2;Second"));
+        }
+
     }
 }
