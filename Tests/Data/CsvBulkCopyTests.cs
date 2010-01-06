63ba15a175181cf761eb98c03ed20b1d2165487e
Torbjorn Gyllebring <i.am@drunkencoder.com>
2010-01-06 09:08:19 +0100

    CollectionDataReader first test added.

--- Tests/Data/CsvBulkCopyTests.cs
+++ Tests/Data/CsvBulkCopyTests.cs
@@ -8,7 +8,7 @@ using Xlnt.Data;
 
 namespace Xlnt.Tests.Data
 {
-    class Row
+    public class Row
     {
         public int Id;
         public string Value;
@@ -23,26 +23,23 @@ namespace Xlnt.Tests.Data
         }
     }
 
-    public class CsvBulkCopyTests
+    public class CsvBulkCopyTests : SqlBulkCopyFixture
     {
-        private const string ConnectionString = "Server=.;Integrated Security=SSPI";
         
         [Test]
         public void Insert_multiple_rows_to_tempdb(){
-            using (var db = new SqlConnection(ConnectionString)){
-                db.Open();
+            WithConnection(db => {
                 var bulkCopy = SqlBulkCopyForRows(db);
                 var data = new CsvDataReader(new StringReader("42,The Answer\r\n7,Sins")) {FieldCount = 2};
 
                 bulkCopy.WriteToServer(data);
 
                 CheckRows(db, new Row { Id = 42, Value = "The Answer" }, new Row { Id = 7, Value = "Sins" });
-            }
+            });
         }
         [Test]
         public void supports_ColumnMapping_by_ordinal(){
-            using (var db = new SqlConnection(ConnectionString)){
-                db.Open();
+            WithConnection(db => {
                 var bulkCopy = SqlBulkCopyForRows(db);
                 var data = new CsvDataReader(new StringReader("The Answer, 42\r\nSins, 7")) { FieldCount = 2 };
 
@@ -51,43 +48,22 @@ namespace Xlnt.Tests.Data
                 bulkCopy.WriteToServer(data);
 
                 CheckRows(db, new Row { Id = 42, Value = "The Answer" }, new Row { Id = 7, Value = "Sins" });
-            }            
+            });
         }
         [Test]
         public void supports_ColumnMapping_by_name()
         {
-            using (var db = new SqlConnection(ConnectionString)){
-                db.Open();
+            WithConnection(db => {
                 var bulkCopy = SqlBulkCopyForRows(db);
-                var data = new CsvDataReader(new StringReader("Value,Id\r\nThe Answer, 42\r\nSins, 7")) {FieldCount = 2};
+                var data = new CsvDataReader(new StringReader("Value,Id\r\nThe Answer, 42\r\nSins, 7")) { FieldCount = 2 };
 
                 data.ReadHeader();
                 bulkCopy.ColumnMappings.Add("Value", "value");
                 bulkCopy.ColumnMappings.Add("Id", "id");
                 bulkCopy.WriteToServer(data);
 
-                CheckRows(db, new Row {Id = 42, Value = "The Answer"}, new Row {Id = 7, Value = "Sins"});
-            }
-        }
-
-        static SqlBulkCopy SqlBulkCopyForRows(SqlConnection db)
-        {
-            using (var command = db.CreateCommand()){
-                command.CommandText = "create table #rows(id int,value varchar(max))";
-                command.ExecuteNonQuery();
-            }
-            return new SqlBulkCopy(db) { DestinationTableName = "#rows" };            
-        }
-
-        static void CheckRows(SqlConnection db, params Row[] expected){
-            using (var command = db.CreateCommand()){
-                command.CommandText = "select id,value from #rows";
-                var rows = new List<Row>();
-                using (var reader = command.ExecuteReader())
-                    while (reader.Read())
-                        rows.Add(new Row {Id = reader.GetInt32(0), Value = reader.GetString(1)});
-                Assert.That(rows, Is.EqualTo(expected));
-            }
+                CheckRows(db, new Row { Id = 42, Value = "The Answer" }, new Row { Id = 7, Value = "Sins" });
+            });
         }
     }
 }
---
+++ Tests/Data/SqlBulkCopyFixture.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Xlnt.Tests.Data
+{
+    public class SqlBulkCopyFixture
+    {
+        const string ConnectionString = "Server=.;Integrated Security=SSPI";
+
+        protected void WithConnection(Action<SqlConnection> action){
+            using(var db = new SqlConnection(ConnectionString)) {
+                db.Open();
+                action(db);
+            }
+        }
+
+        protected static SqlBulkCopy SqlBulkCopyForRows(SqlConnection db) {
+            using(var command = db.CreateCommand()) {
+                command.CommandText = "create table #rows(id int,value varchar(max))";
+                command.ExecuteNonQuery();
+            }
+            return new SqlBulkCopy(db) { DestinationTableName = "#rows" };
+        }
+
+        protected static void CheckRows(SqlConnection db, params Row[] expected) {
+            using(var command = db.CreateCommand()) {
+                command.CommandText = "select id,value from #rows";
+                var rows = new List<Row>();
+                using(var reader = command.ExecuteReader())
+                    while(reader.Read())
+                        rows.Add(new Row { Id = reader.GetInt32(0), Value = reader.GetString(1) });
+                Assert.That(rows, Is.EqualTo(expected));
+            }
+        }
+    }
+}
