d62a07232c5b2d010393ee30664b5f0d3c05d153
Torbjorn Gyllebring <i.am@drunkencoder.com>
2010-01-09 10:24:44 +0100

    Support for passing context between When & Then added.

--- Source/NUnit/Scenario.cs
+++ Source/NUnit/Scenario.cs
@@ -4,26 +4,54 @@ using NUnit.Framework;
 
 namespace Xlnt.NUnit
 {
-    class Scenario : IEnumerable<TestCaseData>
+    class ScenarioBase : IEnumerable<TestCaseData>
     {
-        readonly List<TestCaseData> tests = new List<TestCaseData>();
+        readonly List<TestCaseData> tests;
         string stimuli;
+
+        public ScenarioBase() {
+            this.tests = new List<TestCaseData>();
+        }
+
+        protected ScenarioBase(ScenarioBase other) { this.tests = other.tests; }     
+
+        protected void AddTest(string name, Action action) {
+            tests.Add(new TestCaseData(action).SetName(name));
+        }
+
+        protected string Given(string context) { return "Given " + context; }
+        protected void SetWhen(string stimuli) { this.stimuli = " When " + stimuli; }
+        protected string Then(string happens) { return stimuli + " Then " + happens; }
+
+        IEnumerator<TestCaseData> IEnumerable<TestCaseData>.GetEnumerator() { return tests.GetEnumerator(); }
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() { return tests.GetEnumerator(); }
+    }
+
+    class Scenario : ScenarioBase
+    {
         Action stimulate;
 
+        public Scenario() { }
+        internal Scenario(ScenarioBase other): base(other) { }
+
         public Scenario Given(string context, Action establishContext) {
-            AddTest("Given " + context, establishContext);
+            AddTest(Given(context), establishContext);
             return this;
         }
 
         public Scenario When(string stimuli, Action stimulate) {
-            this.stimuli = " When " + stimuli;
+            SetWhen(stimuli);
             this.stimulate = stimulate;
             return this;
         }
 
+        public Scenario<T> When<T>(string stimuli, Func<T> stimulate) {
+            return new Scenario<T>(this).When(stimuli, stimulate);
+        }
+
         public Scenario Then(string happens, Action check) {
             var thisStimuli = stimulate;
-            AddTest(stimuli + " Then " + happens, () => { thisStimuli(); check(); });
+            AddTest(Then(happens), () => { thisStimuli(); check(); });
             return this;
         }
 
@@ -31,12 +59,24 @@ namespace Xlnt.NUnit
             AddTest("    And " + somethingMore, check);
             return this;
         }
+    }
 
-        void AddTest(string name, Action action) {
-            tests.Add(new TestCaseData(action).SetName(name));
+    class Scenario<T> : ScenarioBase
+    {
+        Func<T> stimulate;
+
+        internal Scenario(ScenarioBase other): base(other) { }
+
+        public Scenario<T> When(string stimuli, Func<T> stimulate) {
+            SetWhen(stimuli);
+            this.stimulate = stimulate;
+            return this;
         }
 
-        IEnumerator<TestCaseData> IEnumerable<TestCaseData>.GetEnumerator() { return tests.GetEnumerator(); }
-        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() { return tests.GetEnumerator(); }
+        public Scenario Then(string happens, Action<T> check) {
+            var thisStimuli = stimulate;
+            AddTest(Then(happens), () => { check(thisStimuli()); });
+            return new Scenario(this);
+        }
     }
 }
