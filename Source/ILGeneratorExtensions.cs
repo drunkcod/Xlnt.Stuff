15b50756d5348a9ebe4c53b7999be74cdc30cfab
Torbjorn Gyllebring <i.am@drunkencoder.com>
2010-01-12 21:17:42 +0100

    ILGeneratorExtensions moved to stuff, static methods and internal classes now marked in checkpoint.

---
+++ Source/ILGeneratorExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection.Emit;
+
+namespace NMeter
+{
+    public static class ILGeneratorExtensions
+    {
+        public static ILGenerator Ldc(this ILGenerator il, int value) {
+            il.Emit(OpCodes.Ldc_I4, value);
+            return il;
+        }
+
+        public static ILGenerator Nop(this ILGenerator il) {
+            il.Emit(OpCodes.Nop);
+            return il;
+        }
+
+        public static ILGenerator Ret(this ILGenerator il) {
+            il.Emit(OpCodes.Ret);
+            return il;
+        }
+    }
+}
