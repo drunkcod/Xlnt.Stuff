39a63040c77ee44a0533b0dac5badcf8a6b10259
Torbjorn Gyllebring <i.am@drunkencoder.com>
2010-01-12 20:12:44 +0100

    Lambdas.Lazy addded.

--- Source/Lambdas.cs
+++ Source/Lambdas.cs
@@ -7,5 +7,16 @@ namespace Xlnt.Stuff
         public static Func<T, object> Box<T, TAny>(Func<T, TAny> func) {
             return x => func(x);
         }
+
+        public static Func<T> Lazy<T>(Func<T> func) {
+            Func<T> forced = null;
+            return () => {
+                if(forced != null)
+                    return forced();
+                var value = func();
+                forced = () => value;
+                return value;
+            };
+        }
     }
 }
---
+++ Tests/LambdasTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Xlnt.Stuff;
+
+namespace Xlnt.Tests
+{
+    [TestFixture]
+    public class LambdasTests
+    {
+        [Test]
+        public void Lazy_only_evaluates_once() {
+            var count = 0;
+            Func<int> fun = () => { ++count; return 1; };
+            var lazy = Lambdas.Lazy(fun);
+
+            lazy(); lazy();
+            Assert.That(count, Is.EqualTo(1));
+        }
+    }
+}
