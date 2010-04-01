03e31909d12cd56ac02c7329b10bdf0e1bb022e8
unknown <drunkcod@.(none)>
2010-04-01 16:57:57 +0200

    Create Serializer when needed instead of holding on to it.

--- Source/Web/Mvc/XmlResult.cs
+++ Source/Web/Mvc/XmlResult.cs
@@ -7,16 +7,9 @@ namespace Xlnt.Web.Mvc
 {
     public class XmlResult : ActionResult
     {
-        XmlSerializer serializer;
         object value;
 
-        public static XmlResult From(object value) {
-            var serializer = new XmlSerializer(value.GetType(), "");
-            return new XmlResult {
-                serializer = serializer,
-                value = value
-            };
-        }
+        public XmlResult(object value) { this.value = value; }
 
         public override void ExecuteResult(ControllerContext context) {
             var response = context.HttpContext.Response;
@@ -25,6 +18,7 @@ namespace Xlnt.Web.Mvc
 
             var ns = new XmlSerializerNamespaces();
             ns.Add("", "");
+            var serializer = new XmlSerializer(value.GetType(), "");
             using (var xml = XmlWriter.Create(response.OutputStream, new XmlWriterSettings {
                 Indent = true,
                 CloseOutput = true
