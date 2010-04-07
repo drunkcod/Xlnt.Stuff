fa4b7f440f84247ded47b7fe75459d806fd84757
drunkcod <i.am@drunkencoder.com>
2010-04-07 13:22:29 +0200

    Additional passthroughs added.

---
+++ Source/Threading/ReaderWriterLockExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace Xlnt.Threading
+{
+    public static class ReaderWriterLockExtensions
+    {
+        public static void Write(this ReaderWriterLock @lock, Action block)
+        {
+            try {
+                @lock.AcquireWriterLock(Timeout.Infinite);
+                block();
+            } finally {
+                @lock.ReleaseWriterLock();
+            }
+        }
+
+        public static T Read<T>(this ReaderWriterLock @lock, Func<T> block)
+        {
+            try {
+                @lock.AcquireWriterLock(Timeout.Infinite);
+                return block();
+            } finally {
+                @lock.ReleaseWriterLock();
+            }
+        }
+    }
+}
