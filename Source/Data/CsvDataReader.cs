62fc1011fe7a5b4da3a963c6bc51190d2160c1bf
drunkcod <i.am@drunkencoder.com>
2010-07-06 13:35:05 +0200

    CsvRecordReader - moved to own file.

--- Source/Data/CsvDataReader.cs
+++ Source/Data/CsvDataReader.cs
@@ -10,14 +10,14 @@ namespace Xlnt.Data
     {
         static char DefaultSeparator = ',';
 
-        readonly RecordReader reader;
+        readonly CsvRecordReader reader;
         string[] values;
         string[] fields;
 
         public CsvDataReader(TextReader reader) : this(reader, DefaultSeparator) { }
 
         public CsvDataReader(TextReader reader, char separator) {
-            this.reader = new RecordReader(reader, separator);
+            this.reader = new CsvRecordReader(reader, separator);
         }
 
         public static CsvDataReader Parse(string s) { return new CsvDataReader(new StringReader(s)); }
@@ -59,92 +59,6 @@ namespace Xlnt.Data
 
         string[] ReadRecord(){
             return reader.Read();
-        }
-
-        class RecordReader : IDisposable
-        {
-            const int MaxFieldLength = 1024;
-            const int MinChunkSize = 256;
-
-            readonly TextReader reader;
-            readonly char separator;
-            readonly char[] buffer = new char[MaxFieldLength + MinChunkSize];
-            int start = 0, read = 0, write = 0, itemsAvailable = 0;
-            char prev, curr = default(char);
-
-            public RecordReader(TextReader reader, char separator) {
-                this.reader = reader;
-                this.separator = separator;
-            }
-
-            public void Dispose() {
-                reader.Dispose();
-            }
-
-            public char Separator { get { return separator; } }
-
-            public string[] Read() {
-                var items = new List<string>();
-                ReadRecord(x => items.Add(new string(x.Array, x.Offset, x.Count)));
-                return items.ToArray();
-            }
-
-            void ReadRecord(Action<ArraySegment<char>> onField) {
-                for (; ; )
-                {
-                    if (!ReadNextChar())
-                        break;
-                    if (curr == '\r')
-                        continue;
-                    if (curr == '\n')
-                        break;
-                    if (curr == Separator) {
-                        onField(new ArraySegment<char>(buffer, start, FieldLength));
-                        read = write = start = write + 1;
-                        continue;
-                    }
-                    if (curr == '"') {
-                        ReadEscaped();
-                        continue;
-                    }
-                    Store(curr);
-                }
-                if (write != 0)
-                    onField(new ArraySegment<char>(buffer, start, FieldLength));
-            }
-
-            int AvailableChunkSpace { get { return buffer.Length - write; } }
-            int FieldLength { get { return write - start; } }
-
-            bool ReadNextChar() {
-                if (itemsAvailable == 0) {
-                    if (AvailableChunkSpace < MinChunkSize) {
-                        write = FieldLength;
-                        Array.Copy(buffer, start, buffer, 0, write);
-                        start = 0;
-                    }
-                    itemsAvailable = reader.Read(buffer, write, AvailableChunkSpace);
-                    if (itemsAvailable == 0)
-                        return false;
-                }
-                --itemsAvailable;
-                prev = curr;
-                curr = buffer[read++];
-                return true;
-            }
-
-            void ReadEscaped() {
-                while (ReadNextChar())
-                {
-                    if (curr == '\\')
-                        continue;
-                    if (curr == '"' && prev != '\\')
-                        return;
-                    Store(curr);
-                }
-            }
-
-            void Store(char ch) { buffer[write++] = ch; }
         }
     }
 }
---
+++ Source/Data/CsvRecordReader.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Xlnt.Data
+{
+    class CsvRecordReader : IDisposable
+    {
+        const int MaxFieldLength = 1024;
+        const int MinChunkSize = 256;
+
+        readonly TextReader reader;
+        readonly char separator;
+        readonly char[] buffer = new char[MaxFieldLength + MinChunkSize];
+        int start = 0, read = 0, write = 0, lastChar = 0;
+        char prev, curr = default(char);
+
+        public CsvRecordReader(TextReader reader, char separator)
+        {
+            this.reader = reader;
+            this.separator = separator;
+        }
+
+        public void Dispose()
+        {
+            reader.Dispose();
+        }
+
+        public char Separator { get { return separator; } }
+
+        public string[] Read()
+        {
+            var items = new List<string>();
+            ReadRecord(() => items.Add(new string(buffer, start, FieldLength)));
+            return items.ToArray();
+        }
+
+        void ReadRecord(Action onField)
+        {
+            while (ReadNextChar())
+            {
+                if (curr == Separator)
+                {
+                    onField();
+                    start = write;
+                }
+                else if (curr == '\r')
+                    continue;
+                else if (curr == '\n')
+                    break;
+                else if (curr == '"')
+                    ReadEscaped();
+                else
+                    Store(curr);
+            }
+            if (write != 0)
+                onField();
+        }
+
+        int AvailableChunkSpace { get { return buffer.Length - write; } }
+        int FieldLength { get { return write - start; } }
+
+        bool ReadNextChar()
+        {
+            if (read == lastChar)
+            {
+                if (AvailableChunkSpace < MinChunkSize)
+                {
+                    read = write = FieldLength;
+                    Array.Copy(buffer, start, buffer, 0, write);
+                    start = 0;
+                }
+                var count = reader.Read(buffer, write, AvailableChunkSpace);
+                if (count == 0)
+                    return false;
+                lastChar = write + count;
+            }
+            prev = curr;
+            curr = buffer[read++];
+            return true;
+        }
+
+        void ReadEscaped()
+        {
+            while (ReadNextChar())
+            {
+                if (curr == '\\')
+                    continue;
+                if (curr == '"' && prev != '\\')
+                    return;
+                Store(curr);
+            }
+        }
+
+        void Store(char ch) { buffer[write++] = ch; }
+    }
+}
