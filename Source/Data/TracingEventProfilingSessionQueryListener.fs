6f61dade0aff9d251a1af6148b7c5ebd58940065
drunkcod <i.am@drunkencoder.com>
2011-07-07 10:36:30 +0200

    Tracing stuff.

--- Source/Data/TracingEventProfilingSessionQueryListener.fs
+++ Source/Data/TracingEventProfilingSessionQueryListener.fs
@@ -1,13 +1,17 @@
 ﻿namespace Xlnt.Data
 
 open System
+open System.Collections.Generic
 open System.Data
+
+type QueryParameters = { ParameterName : String; Value : obj }
 
 type QueryEventArgs(command:IDbCommand, elapsed) =
     inherit EventArgs()    
 
     member this.CommandText = command.CommandText
     member this.Elapsed = elapsed;
+    member this.Parameters = command.Parameters |> Seq.cast<IDataParameter> |> Seq.map (fun x -> { ParameterName = x.ParameterName; Value = x.Value })
 
     override this.ToString() = this.CommandText
 
--- Source/Data/Version.fs
+++ Source/Data/Version.fs
@@ -1,5 +1,5 @@
 ﻿namespace Xlnt.Data
 open System.Reflection
 
-[<assembly:AssemblyVersion("0.0.0.3")>]
+[<assembly:AssemblyVersion("0.0.0.4")>]
 do ()
