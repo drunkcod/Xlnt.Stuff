ff03cec991b59cbfc8e7139829f69a09e69cf0a1
drunkcod <i.am@drunkencoder.com>
2011-12-01 09:45:27 +0100

    TableParameters moved from sample to Xlnt.Data

---
+++ Source/Xlnt.Data/TableParameters.fs
@@ -0,0 +1,26 @@
+﻿namespace Xlnt.Data
+
+open System
+open System.Collections.Generic
+
+type TableParameters(inner : IProfilingSessionQueryListener) =
+    let injectors = Stack()
+
+    new() = TableParameters(NullProfilingSessionListener())
+
+    member this.Add<'a>(xs : 'a seq) = 
+        let newParameter = XmlParameter("@" + typeof<'a>.Name)
+        newParameter.AddRange(xs)
+        injectors.Push(newParameter.Inject)
+        {new IDisposable with member x.Dispose() = injectors.Pop() |> ignore }
+
+    interface IProfilingSessionQueryListener with
+        member this.BeginBatch query = 
+            injectors |> Seq.iter (fun x -> x(query))
+            inner.BeginBatch query
+
+        member this.BeginQuery query = inner.BeginQuery query
+        member this.BeginRow reader = inner.BeginRow reader
+        member this.EndBatch(query, elapsed) = inner.EndBatch(query, elapsed)
+        member this.EndQuery(query, elapsed) = inner.EndQuery(query, elapsed)
+        member this.EndRow(reader, elapsed) = inner.EndRow(reader, elapsed)
--- XmlParameters.Sample.cs
+++ XmlParameters.Sample.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Data.Linq;
 using System.Data.Linq.Mapping;
 using System.Data.SqlClient;
@@ -8,51 +7,6 @@ using Xlnt.Data;
 
 namespace ConsoleApplication23
 {
-    class TableParamaters : IProfilingSessionQueryListener
-    {
-        interface IParameterInjector
-        {
-            void Inject(IDbCommand command);
-        }
-
-        class ParameterScope : IDisposable
-        {
-            readonly TableParamaters parent;
-
-            public ParameterScope(TableParamaters parent) {
-                this.parent = parent;
-            }
-
-            void IDisposable.Dispose() {
-                parent.injectors.Pop();
-            }
-        }
-
-        readonly Stack<Action<IDbCommand>> injectors = new Stack<Action<IDbCommand>>();
-
-        public IDisposable Add<T>(IEnumerable<T> values) {
-            var newParameter = new XmlParameter<T>("@" + typeof(T).Name);
-            newParameter.AddRange(values);
-            injectors.Push(newParameter.Inject);
-            return new ParameterScope(this);
-        }
-
-        public void BeginBatch(ProfiledCommand query) {
-            foreach(var item in injectors)
-                item(query);
-        }         
-
-        public void BeginQuery(ProfiledCommand query) { }
-
-        public void BeginRow(ProfiledDataReader reader) { }
-
-        public void EndBatch(ProfiledCommand query, TimeSpan elapsed) { }
-
-        public void EndQuery(ProfiledCommand query, TimeSpan elapsed) { }
-
-        public void EndRow(ProfiledDataReader reader, TimeSpan elapsed) { }
-    }
-
     [Table(Name = "#MyTable")]
     class MyTable
     {
@@ -63,7 +17,7 @@ namespace ConsoleApplication23
     class Program
     {
         static void Main(string[] args) {
-            var tempTables = new TableParamaters();
+            var tempTables = new TableParameters();
             var session = new DbProfilingSession(tempTables);
             
             var dataContext = new DataContext(session.Connect(new SqlConnection("Server=.;Integrated Security=SSPI")));
